d68666f8eb3beb03b18bfc521e1177b5e75dce1b
notgoogl <notgoog2025@gmail.com>
2025-10-28 13:17:29 -0400

    Update navigation.js

--- query/js/navigation.js
+++ query/js/navigation.js
@@ -6,9 +6,10 @@ searchInput.addEventListener('keypress', function(event) {
         // Prevent the default form submission behavior (if the input is inside a form)
         event.preventDefault(); 
 
-        const url = 'https://www.example.com';
+        const url = './results/';
 
         // Navigate to the constructed URL
         window.location.href = url;
     }
 });
+
